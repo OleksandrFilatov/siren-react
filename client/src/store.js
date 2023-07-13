4a8d92395c0e6a446b8733350a3d25e7448fc942
oleksandr filatov <136085062+OleksandrFilatov@users.noreply.github.com>
2023-07-13 09:46:52 +0300

    push siren

---
+++ client/src/store.js
@@ -0,0 +1,41 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import setAuthToken from './utils/setAuthToken';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+/*
+  NOTE: set up a store subscription listener
+  to store the users token in localStorage
+ */
+
+/*
+  initialize current state from redux store for subscription comparison
+  preventing undefined error
+ */
+let currentState = store.getState();
+
+store.subscribe(() => {
+  // keep track of the previous and current state to compare changes
+  let previousState = currentState;
+  currentState = store.getState();
+  // if the token changes set the value in localStorage and axios headers
+  if (previousState.auth.token !== currentState.auth.token) {
+    const token = currentState.auth.token;
+    setAuthToken(token);
+  }
+});
+
+export default store;
